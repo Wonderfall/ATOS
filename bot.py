8e2b82ab9cdacd43155b17ef323c1d26d75c0321
Wonderfall <wonderfall@targaryen.house>
2020-04-11 03:37:57 +0200

    5.0 wip
    - Introduced auto_mode
    - !start !end commands for tournament

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ with open('data/config.yml', 'r+') as f: config = yaml.safe_load(f)
 if config["system"]["debug"] == True: logging.basicConfig(level=logging.DEBUG)
 
 #### Version
-version                             = "4.21"
+version                             = "5.0-WIP"
 
 ### File paths
 tournoi_path                        = config["paths"]["tournoi"]
