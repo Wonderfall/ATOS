daa938283f5808b6db56cba7964503646275de73
Wonderfall <wonderfall@targaryen.house>
2020-04-17 05:07:38 +0200

    asynchronize http calls

--- utils/http_retry.py
+++ utils/http_retry.py
@@ -1,11 +1,13 @@
 # Retry operation if timeout
+# Also asynchronize operation
 import asyncio
 from requests.exceptions import HTTPError
+from utils.asynchronize import async_wrap
 
 async def http_retry(func, args=[], kwargs={}):
     for retry in range(3):
         try:
-            return func(*args, **kwargs)
+            return await (async_wrap(func))(*args, **kwargs)
         except HTTPError as e:
             if "504" in str(e): await asyncio.sleep(1+retry)
             else: raise HTTPError
