3659f6f5190f9fd123020a5d617d4f03c5b9ea59
Wonderfall <wonderfall@targaryen.house>
2020-05-01 12:50:47 +0200

    update json_stream

--- utils/json_stream.py
+++ utils/json_stream.py
@@ -3,8 +3,8 @@ from utils.json_hooks import int_keys
 from utils.get_config import participants_path
 
 with open(participants_path, 'r+') as f:
-	participants = json.load(f, object_pairs_hook=int_keys)
+    participants = json.load(f, object_pairs_hook=int_keys)
 
 def dump_participants():
-	with open(participants_path, 'w') as f:
-		json.dump(participants, f, indent=4)
+    with open(participants_path, 'w') as f:
+        json.dump(participants, f, indent=4)
